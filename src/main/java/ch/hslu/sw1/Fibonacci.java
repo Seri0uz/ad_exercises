fac30566d59e29470a7359ee4ebf84e00e505e6b
Seri0uz <saemiduss@gmail.com>
2025-02-20 10:58:36 +0100

    create ad_exercises

--- src/main/java/ch/hslu/sw1/Fibonacci.java
+++ src/main/java/ch/hslu/sw1/Fibonacci.java
@@ -6,4 +6,19 @@ import org.slf4j.LoggerFactory;
 public class Fibonacci {
     private static final Logger logger = LoggerFactory.getLogger(Fibonacci.class);
 
+    public static int fiboRec1(int n) {
+        //Rekursionsbasis
+        if ((n == 0) || (n == 1)) {
+            return 1;
+        }
+
+        //Rekursionsvorschrift
+        logger.info("Fibonacci number: " + n);
+        return fiboRec1(n - 1) + fiboRec1(n - 2);
+    }
+
+    public static void main(String[] args) {
+        fiboRec1(13);
+
+    }
 }
