e409be3ed8ef7eddc7fdf1ca164ee4b89798bab0
Samuel <saemiduss@gmail.com>
2025-02-28 13:35:56 +0100

    added StackMachine

--- src/test/java/ch/hslu/sw2/StackMachineTest.java
+++ src/test/java/ch/hslu/sw2/StackMachineTest.java
@@ -16,4 +16,33 @@ class StackMachineTest {
         assertEquals(20,stackMachine.PRINT());
     }
 
+    @Test
+    public void testExerciseB() {
+        StackMachine stackMachine = new StackMachine(new StackArray<>(3));
+        StackMachine stackMachine2 = new StackMachine(new StackArray<>(5));
+        stackMachine.LOAD(2);
+        stackMachine.LOAD(3);
+        stackMachine.ADD();
+
+        stackMachine2.LOAD(4);
+        stackMachine2.LOAD(5);
+        stackMachine2.ADD();
+        stackMachine2.LOAD(stackMachine.PRINT());
+        stackMachine2.MUL();
+        assertEquals(45,stackMachine2.PRINT());
+    }
+
+    @Test
+    public void testExerciseC() {
+        StackMachine stackMachine = new StackMachine(new StackArray<>(7));
+        stackMachine.LOAD(4);
+        stackMachine.LOAD(7);
+        stackMachine.SUB();
+        stackMachine.LOAD(6);
+        stackMachine.DIV();
+        stackMachine.LOAD(5);
+        stackMachine.MUL();
+        assertEquals(10,stackMachine.PRINT());
+    }
+
 }
